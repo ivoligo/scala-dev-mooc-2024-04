9288556fe5132aac41ad7033499b70a71f2c037e
OLIvanov <OLIvanov@systematic.ru>
2024-05-05 17:03:47 +0300

    небольшие задания:     функция проверки четности числа;     функция проверки нечетности числа;     функция возвращающая массив четных чисел;     функция возвращающая массив нечетных чисел;

--- src/main/scala/ru/otus/module1/01-functions.scala
+++ src/main/scala/ru/otus/module1/01-functions.scala
@@ -54,24 +54,29 @@ object functions {
    *  Задание 1. Написать ф-цию метод isEven, которая будет вычислять является ли число четным
    */
 
+  def isEven(x:Int): Boolean = if (x % 2 == 0) true else false
 
+  // почему не дает написать через val (подчеркивает параметр x)
+  // val isEven: Int => Boolean = if (x % 2 == 0) true else false
   /**
    * Задание 2. Написать ф-цию метод isOdd, которая будет вычислять является ли число нечетным
    */
 
+  def isOdd(x:Int): Boolean = if (x % 2 != 0) true else false
 
   /**
    * Задание 3. Написать ф-цию метод filterEven, которая получает на вход массив чисел и возвращает массив тех из них,
    * которые являются четными
    */
 
-
+  def filterEven(arr: Array[Int]): Array[Int] = for (elem <- arr if (elem % 2 == 0) ) yield elem
 
   /**
    * Задание 4. Написать ф-цию метод filterOdd, которая получает на вход массив чисел и возвращает массив тех из них,
    * которые являются нечетными
    */
 
+  def filterOdd(arr: Array[Int]): Array[Int] = for (elem <- arr if (elem % 2 != 0) ) yield elem
 
   /**
    * return statement
